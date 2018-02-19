bbe4624d22cec1277eb828f8d03b8b5c7a7c98f1
Ftz <fontez@gmail.com>
2018-02-19 23:15:43 +0000

    Initial Commit

---
+++ Quaddro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quaddro
+//
+//  Created by Ftz on 19/02/18.
+//  Copyright © 2018 Ftz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
